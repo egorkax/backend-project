9b06c6792d577a3945993d8c44fca1ac1f7e087e
EgorKokhnenko <kax.egor@mail.ru>
2022-11-22 17:42:27 +0300

    create middleware

--- src/routes/products-router.ts
+++ src/routes/products-router.ts
@@ -1,8 +1,14 @@
 import {Request, Response, Router} from "express";
 import {productsRepository} from "../repositories/products-repository";
+import {body, validationResult} from "express-validator";
+import {inputValidationMiddleware} from "../midlewares/input-validation-middleware";
 
 export const productRouter = Router({})
 
+const titleValidation = body('title').trim().isLength({
+    min: 2,
+    max: 12
+}).withMessage('Title should be from 2 to 12 symbols ')
 
 productRouter.get('/', (req: Request, res: Response) => {
     let searchString = req.query.title?.toString()
@@ -21,24 +27,23 @@ productRouter.get('/:id', (req: Request, res: Response) => {
 productRouter.delete('/:id', (req: Request, res: Response) => {
     let uriParams = +req.params.id
     let isDelete = productsRepository.deleteProduct(uriParams)
-    if(isDelete)res.status(200)
+    if (isDelete) res.status(200)
     else res.status(400)
 
 })
 
-productRouter.post('/', (req: Request, res: Response) => {
+productRouter.post('/', titleValidation, inputValidationMiddleware, (req: Request, res: Response) => {
     let newTitle = req.body.title
     let product = productsRepository.createProduct(newTitle)
-    if(product)res.status(200).send(product)
-    else res.status(400)
+    res.status(200).send(product)
 })
 
-productRouter.put('/:id', (req: Request, res: Response) => {
+productRouter.put('/:id', titleValidation,inputValidationMiddleware, (req: Request, res: Response) => {
     let uriParams = +req.params.id
     let newTitle = req.body.title
     let isUpdated = productsRepository.updateProduct(newTitle, uriParams)
     if (isUpdated) {
         const product = productsRepository.findProduct(uriParams)
         res.status(200).send(product)
-    }else res.status(404)
+    } else res.status(404)
 })
