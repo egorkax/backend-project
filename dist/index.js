1c045a2e3de0efbdd638b6d9053b7ff5f47abf57
EgorKokhnenko <kax.egor@mail.ru>
2022-11-27 17:16:35 +0300

    create socket connection

--- dist/index.js
+++ dist/index.js
@@ -1,30 +1,52 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
+// import express from 'express'
+// import bodyParser from "body-parser";
+// import {productRouter} from "./routes/products-router";
+// import {runDb} from "./repositories/db-repositories";
+//
+//
+// const app = express()
+// const port = process.env.PORT || 3000
+//
+// const parserMiddleware = bodyParser({})
+// app.use(parserMiddleware)
+//
+// app.use('/products', productRouter)
+// const startApp = async () => {
+//     await runDb()
+//     app.listen(port, () => {
+//         console.log(`Example app listening on port ${port}`)
+//     })
+// }
+// startApp()
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+//WEB-SOCKET
 const express_1 = __importDefault(require("express"));
-const body_parser_1 = __importDefault(require("body-parser"));
-const products_router_1 = require("./routes/products-router");
-const db_repositories_1 = require("./repositories/db-repositories");
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
 const app = (0, express_1.default)();
-const port = 3000;
-const parserMiddleware = (0, body_parser_1.default)({});
-app.use(parserMiddleware);
-app.use('/products', products_router_1.productRouter);
-const startApp = () => __awaiter(void 0, void 0, void 0, function* () {
-    yield (0, db_repositories_1.runDb)();
-    app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`);
-    });
+const server = http_1.default.createServer(app);
+const socket = new socket_io_1.Server(server, { cors: { origin: "*" } });
+const port = process.env.PORT || 3007;
+const messages = [
+    { message: 'Hello Polina', id: '2342352fs', user: { id: 'sdff243', name: 'Egor' } },
+    { message: 'Hello Egor', id: '23234122fs', user: { id: 'scbg123', name: 'Polina' } },
+];
+app.get('/', (req, res) => {
+    res.send('Hello, its WEB SOCKET SERVER');
 });
-startApp();
+socket.on('connection', (socketChannel) => {
+    socketChannel.on('client-message-sent', (message) => {
+        let messItem = { message: message, id: 'sdf' + new Date().getTime(), user: { id: 'asdfasdf13e', name: 'New' } };
+        messages.push(messItem);
+        socket.emit('new-message-sent', messItem);
+    });
+    socketChannel.emit('init-messages-published', messages);
+    console.log('a user connected');
+});
+server.listen(port, () => {
+    console.log('listening on *:3007');
+});
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,56 @@
+// import express from 'express'
+// import bodyParser from "body-parser";
+// import {productRouter} from "./routes/products-router";
+// import {runDb} from "./repositories/db-repositories";
+//
+//
+// const app = express()
+// const port = process.env.PORT || 3000
+//
+// const parserMiddleware = bodyParser({})
+// app.use(parserMiddleware)
+//
+// app.use('/products', productRouter)
+// const startApp = async () => {
+//     await runDb()
+//     app.listen(port, () => {
+//         console.log(`Example app listening on port ${port}`)
+//     })
+// }
+// startApp()
+
+
+//WEB-SOCKET
+
 import express from 'express'
-import bodyParser from "body-parser";
-import {productRouter} from "./routes/products-router";
-import {runDb} from "./repositories/db-repositories";
+import http from "http";
+import {Server} from 'socket.io'
 
 const app = express()
-const port = 3000
+const server = http.createServer(app);
+const socket = new Server(server, {cors: {origin: "*"}});
+const port = process.env.PORT || 3007
+
+const messages = [
+    {message: 'Hello Polina', id: '2342352fs', user: {id: 'sdff243', name: 'Egor'}},
+    {message: 'Hello Egor', id: '23234122fs', user: {id: 'scbg123', name: 'Polina'}},
+]
 
 
-const parserMiddleware = bodyParser({})
-app.use(parserMiddleware)
+app.get('/', (req, res) => {
+    res.send('Hello, its WEB SOCKET SERVER');
+});
 
-app.use('/products', productRouter)
-
-
-
-const startApp =async () => {
-    await runDb()
-    app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`)
+socket.on('connection', (socketChannel) => {
+    socketChannel.on('client-message-sent', (message) => {
+        let messItem={message: message, id: 'sdf'+ new Date().getTime(), user: {id: 'asdfasdf13e', name: 'New'}}
+        messages.push(messItem)
+        socket.emit('new-message-sent',messItem)
     })
-}
-startApp()
+    socketChannel.emit('init-messages-published', messages)
+    console.log('a user connected');
+});
+
+server.listen(port, () => {
+    console.log('listening on *:3007');
+});
